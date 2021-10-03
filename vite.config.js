dcc97557d1a57d1edce8a3306be9431a733dfa16
SouWinds <pingxi8@dingtalk.com>
2021-10-03 08:29:47 +0800

    修改：项目部署

--- vite.config.js
+++ vite.config.js
@@ -33,5 +33,5 @@ export default defineConfig({
   // https: false, // 是否开启 https
   // ssr: false, // 服务端渲染
   base: './', // 生产环境下的公共路径
-  // outDir: 'dist', // 打包构建输出路径，默认 dist ，如果路径存在，构建之前会被删除
+  outDir: 'docs', // 打包构建输出路径，默认 dist ，如果路径存在，构建之前会被删除
 })
